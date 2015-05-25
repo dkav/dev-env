fb3922f535fc91ac30682ae02407887ac63dda9b
Darren Kavanagh <dkav@users.noreply.github.com>
2015-05-24 18:44:02 -0700

    Reconfigured Node development setup

--- node_setup.sh
+++ node_setup.sh
@@ -1,3 +1,14 @@
-# Setup Node.js
-brew install node
+# Setup Node development environment
+
+# Install Node Version Manager
+brew install nvm
+mkdir /usr/local/nvm
+cp $(brew --prefix nvm)/nvm-exec /usr/local/nvm/
+
+# Make stable release the default
+source ~/.zprofile
+nvm alias default stable
+
+# Add global packages
 npm install -g topojson
+
