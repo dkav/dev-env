f572ef911ff11077329c5f6a71e05ee6585722f6
Darren Kavanagh <dkav@users.noreply.github.com>
2016-02-21 00:09:45 -0800

    Refer to Jupyter and add pandas
    Following project direction, refer to Jupyter rather than IPython.
    
    Add Python Data Analysis Library (pandas) to list of libraries to
    install.

--- Python/py_jupyter.sh
+++ Python/py_jupyter.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Summary: Setup iPython virutal environment
+# Summary: Setup Jupyter virutal environment
 #
-# Usage: py_ipython <Python version>
+# Usage: py_jupyter <Python version>
 #
 
 py_version=$1  # Python version to use
 
 if [ -z "$py_version" ]; then
-    echo "Usage: py_ipython <Python version>"
+    echo "Usage: py_jupyter <Python version>"
 else
    . ~/.bash_profile
     pydev
 
-    if pyenv virtualenv $py_version ipython; then
+    if pyenv virtualenv $py_version jupyter; then
         pyenv rehash
 
-        pyenv shell ipython
+        pyenv shell jupyter
 
         pip install jupyter
         brew install freetype
         pip install matplotlib
+        pip install pandas
         pip install rpy2 # R needs to be installed
 
         pyenv shell system
