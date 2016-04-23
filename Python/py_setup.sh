9ee21c6cc24199b3630d34af19819cafbe60e113
Darren Kavanagh <dkav@users.noreply.github.com>
2016-04-23 13:44:29 -0700

    Remove Python compile flags
    CFLAGS and CPPFLAGS are no longer required to compile Python via pyenv.
    The flags were originally introduced to eliminate compile errors.

--- Python/py_setup.sh
+++ Python/py_setup.sh
@@ -14,7 +14,7 @@ else
 
     for py_version in "$@"
     do
-        CFLAGS="-I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib" CPPFLAGS="-I$(brew --prefix openssl)/include" pyenv install $py_version
+        pyenv install $py_version
     done
     pyenv rehash
 fi
