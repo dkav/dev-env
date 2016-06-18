bbe524a576dbe5d29cd3e87cd8b7f72c36e9ec71
Darren Kavanagh <dkav@users.noreply.github.com>
2016-06-18 16:47:05 -0700

    Add script to update npm packages
    Script updates all outdated global npm packages. The script came from a
    gist that was referenced in the ‘Updating global packages’ npm help
    page.

---
+++ Node/npm_update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Source - https://gist.github.com/othiym23/4ac31155da23962afd0e
+
+# Summary: Updates outdated global packages
+#
+#
+# Usage: npm_update.sh
+#
+
+set -e
+set -x
+
+# Activate Node Version Manager
+. ~/.bash_profile
+nodev
+
+for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2)
+do
+    npm -g install "$package"
+done
