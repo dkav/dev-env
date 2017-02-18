6486396bf49ba566c43a0c8f7d39f54762be8d48
Darren Kavanagh <dkav@users.noreply.github.com>
2017-02-17 20:38:26 -0800

    Add script to install Xcode solarized theme

---
+++ Xcode/install_solarized.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Solarized Dark Theme for Xcode  #
+#=========================================#
+echo "Installing Solarized Theme"
+
+DESTDIR=~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+mkdir -p $DESTDIR
+
+curl -o "$DESTDIR/Solarized Dark.dvtcolortheme" https://raw.githubusercontent.com/ArtSabintsev/Solarized-Dark-for-Xcode/master/Solarized_Dark_by_ArtSabintsev.dvtcolortheme
