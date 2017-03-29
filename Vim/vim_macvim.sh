24d3dcf4a19a9b104d52c4b94acd8e75c66509f2
Darren Kavanagh <dkav@users.noreply.github.com>
2017-03-29 00:14:25 -0700

    Use mvim in application folder
    Use mvim script that is now available (as of MacVim Snapshot 129) in
    MacVim.app/Contents/bin/mvim folder.

--- Vim/vim_macvim.sh
+++ Vim/vim_macvim.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Allow easy calling of MacVim from command line
-cp mvim /usr/local/bin/
-ln -s /usr/local/bin/mvim /usr/local/bin/mvimdiff
-ln -s /usr/local/bin/mvim /usr/local/bin/mview
-ln -s /usr/local/bin/mvim /usr/local/bin/mex
-ln -s /usr/local/bin/mvim /usr/local/bin/rmvim
+ln -s /Applications/MacVim.app/Contents/bin/mvim /usr/local/bin/mvim
+ln -s /Applications/MacVim.app/Contents/bin/mvim /usr/local/bin/mvimdiff
+ln -s /Applications/MacVim.app/Contents/bin/mvim /usr/local/bin/mview
