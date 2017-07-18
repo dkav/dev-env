e20067b979ab25b6fa406e4ea720bfb1c86fcfed
Darren Kavanagh <dkav@users.noreply.github.com>
2017-07-18 16:37:04 -0700

    Update docker zsh completion
    Docker app now includes zsh completions. Create symbolic link like bash completions.

--- Docker/docker_complete.sh
+++ Docker/docker_complete.sh
@@ -4,17 +4,16 @@
 #
 # Usage: docker_complete
 
-# Bash completions
 dockerdir="/Applications/Docker.app/Contents/Resources/etc"
+
+# Bash completions
 cd /usr/local/etc/bash_completion.d
-ln -s $dockerdir/docker.bash-completion
-ln -s $dockerdir/docker-machine.bash-completion
-ln -s $dockerdir/docker-compose.bash-completion
+ln -sf $dockerdir/docker.bash-completion
+ln -sf $dockerdir/docker-machine.bash-completion
+ln -sf $dockerdir/docker-compose.bash-completion
 
 # Zsh completions
-zshdir="/usr/local/share/zsh/site-functions"
-curl -fLo $zshdir/_docker https://github.com/docker/docker/raw/master/contrib/completion/zsh/_docker
-
-curl -L https://raw.githubusercontent.com/docker/machine/v$(docker-machine --version | tr -ds ',' ' ' | awk 'NR==1{print $(3)}')/contrib/completion/zsh/_docker-machine > $zshdir/_docker-machine
-
-curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > $zshdir/_docker-compose
+cd /usr/local/share/zsh/site-functions
+ln -sf $dockerdir/docker.zsh-completion _docker
+ln -sf $dockerdir/docker-machine.zsh-completion _docker-machine
+ln -sf $dockerdir/docker-compose.zsh-completion _docker-compose
