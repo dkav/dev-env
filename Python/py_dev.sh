4e11643b70a50065d268443d31ebfe56f23c18d9
Darren Kavanagh <dkav@users.noreply.github.com>
2019-09-04 15:27:42 -0700

    Add pipdeptree and ipython to pip install

--- Python/py_dev.sh
+++ Python/py_dev.sh
@@ -2,4 +2,11 @@
 
 # Summary: Install base packages for Python development
 
+# Package tools
+pip install pipdeptree
+
+# Shell tools
+pip install ipython
+
+# Linting tools
 pip install flake8 flake8_docstrings pep8-naming pylint vulture
