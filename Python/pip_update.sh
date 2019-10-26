6d8d2a88c6c302f1a8682214a494a06466f4a733
Darren Kavanagh <dkav@users.noreply.github.com>
2019-10-25 17:29:44 -0700

    Make pip install output quiet

--- Python/pip_update.sh
+++ Python/pip_update.sh
@@ -3,5 +3,8 @@
 # Summary: Update all outdated Python packages
 #
 
-pip3 list --outdated --format=columns | tail -n +3 \
-            | cut -d ' ' -f 1 | xargs -n 1 pip3 install -U
+pip3 list --outdated \
+            | tee "$(tty)" \
+            | tail -n +3 \
+            | cut -d ' ' -f 1 \
+            | xargs -n 1 pip3 install --upgrade --quiet
