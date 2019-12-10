a578d5de5885f85bce347b2815c3533e8748d7e3
Darren Kavanagh <dkav@users.noreply.github.com>
2019-12-10 11:24:42 -0800

    Reduce screen output

--- Ruby/gem_update.sh
+++ Ruby/gem_update.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Summary: Update base packages for Ruby development
-
-gem update --system
-gem update bundler github-pages
-gem cleanup
+gem update --system --silent
+gem outdated | egrep "bundler|github-pages" \
+	&& gem --silent update bundler github-pages
+gem cleanup --silent
