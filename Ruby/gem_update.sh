c2561defac8c9c4332ef8e7b6d579ff9d5384278
Darren Kavanagh <dkav@users.noreply.github.com>
2019-12-15 16:40:53 -0800

    Fix gem update command

--- Ruby/gem_update.sh
+++ Ruby/gem_update.sh
@@ -3,5 +3,5 @@
 # Summary: Update base packages for Ruby development
 gem update --system --silent
 gem outdated | egrep "bundler|github-pages" \
-	&& gem --silent update bundler github-pages
+	&& gem update --silent bundler github-pages
 gem cleanup --silent
