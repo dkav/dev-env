d25e3b3cce369f3c4a7229c60a892ff535a1068c
Darren Kavanagh <dkav@users.noreply.github.com>
2020-02-14 18:08:27 -0800

    Add script that updates all environments

---
+++ link-update.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+source_dir=${0:a:h}
+
+ln -s -f $source_dir/mac-update.sh   /usr/local/bin/mac-update
---
+++ mac-update.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+#
+# Update macOS environment
+
+denv_dir=${"$(readlink $0)":a:h}
+
+# Homebrew
+echo "Updating Homebrew..."
+"$denv_dir/Homebrew/brew-update.sh"
+
+# Vim
+echo "\nUpdating Vim Plugins..."
+"$denv_dir/vim/vim-update.sh"
+
+# Python
+echo "\nUpdating Python Packages..."
+"$denv_dir/Python/pip-update.sh"
+
+# Ruby
+echo "\nUpdating Ruby Gems..."
+"$denv_dir/Ruby/gem-update.sh"
+
+# Node
+echo "\nUpdating Node Packages..."
+"$denv_dir/Node/npm-update.sh"
