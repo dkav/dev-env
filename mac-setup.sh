4ce34c3d18fd689af067cac7bfe8505b453be375
Darren Kavanagh <dkav@users.noreply.github.com>
2020-02-14 19:35:55 -0800

    Add script to setup macOS environment

---
+++ mac-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+#
+# Setup macOS environment.
+
+denv_dir=${"$(readlink $0)":a:h}
+
+# Homebrew
+echo "Setup Homebrew..."
+make -f "$denv_dir/Homebrew/Makefile"
+
+# Vim
+echo "\nSetup Vim ..."
+"$denv_dir/vim/vim-setup.sh"
+"$denv_dir/vim/MacVim Install.command"
+
+# Python
+echo "\nSetup Python Packages..."
+"$denv_dir/Python/py-dev.sh"
+"$denv_dir/Python/py-jupyter.sh"
+
+# Ruby
+echo "\nSetup Ruby Gems..."
+"$denv_dir/Ruby/rb-dev.sh"
+
+# Node
+echo "\nSetup Node Packages..."
+"$denv_dir/Node/npm-packages.sh"
