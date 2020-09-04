07ef532f555e9d2de82d06bbdd4c0c2770c1bc9a
Darren Kavanagh <dkav@users.noreply.github.com>
2020-09-04 16:13:21 -0700

    Simplify Homebrew update

--- Homebrew/brew-update.sh
+++ Homebrew/brew-update.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 #
-# Update Homebrew and Homebrew packages.
+# Update Homebrew packages.
 
 brew update 1>/dev/null
 
@@ -9,18 +9,6 @@ if [[ -n $bo ]]; then
   echo "Updating packages"
   echo $bo
   brew upgrade 1>/dev/null
-fi
-
-bko=$(brew cask outdated --verbose)
-if [[ -n $bko ]]; then
-  echo "Updating cask packages"
-  echo $bko
-  brew cask upgrade 1>/dev/null
-fi
-
-if [[ -n $bo || -n $bko ]]; then
-  echo "Cleaning up"
-  brew cleanup 1>/dev/null
 else
   echo "No packages to update"
 fi
