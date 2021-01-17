caa82b74e4a5fc1c5802daef9c424014f4f2f91a
Darren Kavanagh <dkav@users.noreply.github.com>
2021-01-17 01:37:51 -0800

    Ignore npm updates

--- Node/npm-update.sh
+++ Node/npm-update.sh
@@ -6,7 +6,7 @@
 # https://gist.github.com/othiym23/4ac31155da23962afd0e
 
 if [ -x "$(command -v npm)" ]; then
-  no=($(npm --global outdated --parseable --depth=0 | cut -d: -f3,4))
+  no=($(npm --global outdated --parseable --depth=0 | sed  '/npm@/d' | cut -d: -f3,4 ))
   if [[ -n $no ]]; then
     for package in $no; do
       npkg="$(cut -d: -f2 <<<"$package")"
@@ -14,12 +14,7 @@ if [ -x "$(command -v npm)" ]; then
       pver="$(cut -d@ -f2 <<<"$npkg")"
       pkg="$(cut -d@ -f1 <<<"$npkg")"
       echo "Updating $pkg ($cver-->$pver)"
-      if [[ $npkg =~ "^npm@" ]]; then
-        npm install --force --global --no-progress --quiet \
-          $npkg 1>/dev/null
-      else
-        npm install --global --no-progress --quiet $npkg 1>/dev/null
-      fi
+      npm install --global --no-progress --quiet $npkg 1>/dev/null
     done
   else
     echo "No packages to update"
