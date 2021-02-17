032c7a9f37bba8b46333132302fdcfc5509f30c1
Darren Kavanagh <dkav@users.noreply.github.com>
2021-02-17 13:06:08 -0800

    Refactor gem update

--- Ruby/rb-gems-update.sh
+++ Ruby/rb-gems-update.sh
@@ -2,20 +2,17 @@
 #
 # Update base packages for Ruby development.
 
-if [ -x "$(command -v /usr/local/opt/ruby/bin/gem)" ]; then
+if [ -x "$(command -v /usr/local/opt/ruby/bin/bundle)" ]; then
   echo "Updating Ruby Gems..."
   cd ${0:A:h}
-  gem update --system --quiet 1>/dev/null
-  if [ -x "$(command -v /usr/local/opt/ruby/bin/bundle)" ]; then
-    export GEM_PATH=$(gem environment gemdir)
-    bundle update --all | grep "Installing"
-    if [[ $? != 0 ]]; then
+  gem update --system --quiet | grep "Installing"
+  gs_update=$?
+  export GEM_PATH=$(gem environment gemdir)
+  bundle update --all | grep "Installing"
+  if [[  $gs_update != 0 && $? != 0  ]]; then
       echo "No gems updated"
-    fi
-    bundle clean --force
-  else
-    echo "Error: bundle is not installed" >&2
   fi
+  bundle clean --force
 else
-  echo "Error: gem is not installed" >&2
+  echo "Error: bundle is not installed" >&2
 fi
