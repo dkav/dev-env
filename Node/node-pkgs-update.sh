a1a6168ea387f30ef192648ed217486ff7e11ab0
Darren Kavanagh <dkav@users.noreply.github.com>
2021-02-23 12:15:12 -0800

    Change command option order

--- Node/node-pkgs-update.sh
+++ Node/node-pkgs-update.sh
@@ -7,7 +7,7 @@
 
 if [ -x "$(command -v npm)" ]; then
   echo "Updating Node Packages..."
-  no=($(npm --global outdated --parseable --depth=0 --quiet | cut -d: -f3,4))
+  no=($(npm outdated --global --parseable --depth=0 --quiet | cut -d: -f3,4))
   if [[ -n $no ]]; then
     for package in $no; do
       npkg="$(cut -d: -f2 <<<"$package")"
