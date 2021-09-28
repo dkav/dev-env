37c067e5f997aca218ca135ef20cb2b9c0a4f5cd
Darren Kavanagh <dkav@users.noreply.github.com>
2021-09-28 10:10:10 -0700

    Add vim-commentary and remove nerdcommenter

--- vim/vplugins.sh
+++ vim/vplugins.sh
@@ -3,7 +3,7 @@
 # List of Vim plugins.
 
 plugins=(airline airline_themes flattened nerdtree tagbar gutentags fugitive \
-	gitgutter ale nerdcommenter ctrlp)
+	commentary gitgutter ale ctrlp )
 
 
 # Array=("GitHub repository" "local Vim plugin folder" "Make Vim docs")
@@ -21,7 +21,7 @@ nerdtree=("scrooloose/nerdtree" "ide/start/nerdtree" true)
 tagbar=("majutsushi/tagbar" "ide/start/tagbar" true)
 gutentags=("ludovicchabant/vim-gutentags" "ide/start/gutentags" true)
 fugitive=("tpope/vim-fugitive" "ide/start/fugitive" true)
+commentary=("tpope/vim-commentary" "ide/start/commentary" true)
 gitgutter=("airblade/vim-gitgutter" "ide/start/gitgutter" true)
 ale=("dense-analysis/ale" "ide/start/ale" true)
-nerdcommenter=("scrooloose/nerdcommenter" "ide/opt/nerdcommenter" true)
 ctrlp=("ctrlpvim/ctrlp.vim" "ide/opt/ctrlp" true)
