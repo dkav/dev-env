1a5795e41321ffda48f009ab5b0a2b239921e776
Darren Kavanagh <dkav@users.noreply.github.com>
2021-09-30 21:17:27 -0700

    Allow install and uninstall of single vim plugin

--- vim/vim-plugins-install.sh
+++ vim/vim-plugins-install.sh
@@ -2,8 +2,6 @@
 #
 # Install Vim plugins.
 
-echo "Installing vim plugins..."
-
 dotdir=$HOME/.vim
 packdir=$dotdir/pack
 sdir=${0:A:h}
@@ -19,25 +17,40 @@ clone_vplugin()
   local install_dir=$packdir/$2
   local gen_doc=$3
 
-  echo >&2 "    $(basename $repo)"
+  if [ ! -d "$install_dir"/.git ]; then
+    echo "    $(basename $repo)"
 
-  # Get project from Github
-  git clone --depth 1 --quiet git@github.com:/$repo.git $install_dir
+    # Get project from Github
+    git clone --depth 1 --quiet git@github.com:/$repo.git $install_dir
 
-  # Generate helptags
-  if ( $gen_doc ) ; then
-    mvim -nNes -u NONE -c "helptags $install_dir/doc" -c q
+    # Generate helptags
+    if ( $gen_doc ) ; then
+      mvim -nNes -u NONE -c "helptags $install_dir/doc" -c q
+    fi
+  else
+    echo >&2 "    $(basename $repo) already installed"
   fi
 }
 
 # Clone
 source $sdir/vplugins.sh
-for plugin in $plugins; do
-  clone_vplugin ${(P)plugin}
-done
 
-# Install Powerline font
-if [[ ${plugins[(ie)airline]} -le ${#plugins} ]]; then
-  echo
-  source $sdir/vim-font.sh
+if [[ -n $1 ]]; then
+  if [[ ${plugins[(ie)$1]} -le ${#plugins} ]]; then
+    echo "Installing vim plugin..."
+    clone_vplugin ${(P)1}
+  else
+    echo "Plugin not listed in vplugins.sh"
+  fi
+else
+  echo "Installing vim plugins..."
+  for plugin in $plugins; do
+    clone_vplugin ${(P)plugin}
+  done
+
+  # Install Powerline font
+  if [[ ${plugins[(ie)airline]} -le ${#plugins} ]]; then
+    echo
+    source $sdir/vim-font.sh
+  fi
 fi
--- vim/vim-plugins-uninstall.sh
+++ vim/vim-plugins-uninstall.sh
@@ -2,5 +2,17 @@
 #
 # Uninstall Vim plugins.
 
-echo "Removing vim plugins"
-rm -frd $HOME/.vim/pack
+packdir=$HOME/.vim/pack
+
+if [[ -n $1 ]]; then
+  source ${0:A:h}/vplugins.sh
+  if [[ ${plugins[(ie)$1]} -le ${#plugins} ]]; then
+    echo "Removing vim plugin $1"
+    rm -frd ${packdir:?}/${${(P)1}[2]}
+  else
+    echo "Plugin not listed in vplugins.sh"
+  fi
+else
+  echo "Removing vim plugins"
+  rm -frd $packdir
+fi
