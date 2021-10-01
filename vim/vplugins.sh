6c21b037cfaef4df33e6e1257984ecf89b130cbf
Darren Kavanagh <dkav@users.noreply.github.com>
2021-09-30 21:15:52 -0700

    Include vim-surround plugin

--- vim/vplugins.sh
+++ vim/vplugins.sh
@@ -3,7 +3,7 @@
 # List of Vim plugins.
 
 plugins=(airline airline_themes flattened nerdtree tagbar gutentags fugitive \
-	commentary gitgutter ale ctrlp )
+	commentary surround gitgutter ale ctrlp )
 
 
 # Array=("GitHub repository" "local Vim plugin folder" "Make Vim docs")
@@ -22,6 +22,7 @@ tagbar=("majutsushi/tagbar" "ide/start/tagbar" true)
 gutentags=("ludovicchabant/vim-gutentags" "ide/start/gutentags" true)
 fugitive=("tpope/vim-fugitive" "ide/start/fugitive" true)
 commentary=("tpope/vim-commentary" "ide/start/commentary" true)
+surround=("tpope/vim-surround" "ide/start/surround" true)
 gitgutter=("airblade/vim-gitgutter" "ide/start/gitgutter" true)
 ale=("dense-analysis/ale" "ide/start/ale" true)
 ctrlp=("ctrlpvim/ctrlp.vim" "ide/opt/ctrlp" true)
