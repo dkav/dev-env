8df5fb5c2ea2e5ec7debd8881cfe88113f048229
Darren Kavanagh <dkav@users.noreply.github.com>
2022-07-31 15:26:38 -0700

    Remove white space

--- python/py-pkgs-uninstall.sh
+++ python/py-pkgs-uninstall.sh
@@ -4,7 +4,7 @@
 
 if [ -x "$(command -v /usr/local/opt/python@3.10/bin/pip3)" ]; then
   echo "Uninstalling Python packages..."
-  pip3 freeze | xargs pip3 uninstall --yes --quiet  
+  pip3 freeze | xargs pip3 uninstall --yes --quiet
 else
   echo "Error: pip is not installed" >&2
 fi
