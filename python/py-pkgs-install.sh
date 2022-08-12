1e90b3162eff89cc7d163b256ecfca34dbd7e47e
Darren Kavanagh <dkav@users.noreply.github.com>
2022-08-12 12:53:54 -0700

    Revert "Use Homebrew Python 3.10"
    This reverts commit e625fb1176d816199a31bdfeb87e82a2a9fba954.

--- python/py-pkgs-install.sh
+++ python/py-pkgs-install.sh
@@ -2,7 +2,7 @@
 #
 # Install base packages for Python.
 
-if [ -x "$(command -v /usr/local/opt/python@3.10/bin/pip3)" ]; then
+if [ -x "$(command -v /usr/local/bin/pip3)" ]; then
   echo "Installing Python packages..."
   pip3 install --requirement ${0:a:h}/py-requirements.txt --quiet
 else
