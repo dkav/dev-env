7c4c0d93a7e66f11ae320e34b5cbe1aaaab37d9d
Darren Kavanagh <dkav@users.noreply.github.com>
2022-11-12 20:37:31 -0800

    Ignore pip outdated message

--- python/py-pkgs-install.sh
+++ python/py-pkgs-install.sh
@@ -4,7 +4,7 @@
 
 if [ -x "$(command -v /usr/local/bin/pip3)" ]; then
   echo "Installing Python packages..."
-  pip3 install --requirement ${0:a:h}/py-requirements.txt --quiet
+  pip3 install --requirement ${0:a:h}/py-requirements.txt --disable-pip-version-check --quiet
 else
   echo "Error: pip is not installed" >&2
 fi
--- python/py-pkgs-uninstall.sh
+++ python/py-pkgs-uninstall.sh
@@ -4,6 +4,7 @@
 
 if [ -x "$(command -v /usr/local/bin/pip3)" ]; then
   echo "Uninstalling Python packages..."
+  export PIP_DISABLE_PIP_VERSION_CHECK=1
   pip3 freeze | xargs pip3 uninstall --yes --quiet
 else
   echo "Error: pip is not installed" >&2
