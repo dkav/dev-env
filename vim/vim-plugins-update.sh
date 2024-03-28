fd02c29b9314a32553c195b0770f9014b97b6a65
Darren Kavanagh <dkav@users.noreply.github.com>
2024-03-28 00:00:52 -0700

    vim: add carriage return at start of printf
    Adding carriage return at start of printf keeps caret at end of line until next printf statement.

--- vim/vim-plugins-update.sh
+++ vim/vim-plugins-update.sh
@@ -32,7 +32,7 @@ update_vplugin()
     upstream_hash=$(git rev-parse --quiet --verify --short "$def_branch@{upstream}")
 
     if [ "$head_hash" != "$upstream_hash" ]; then
-      printf "Updating %s (%s --> %s)\033[0K\r\n" \
+      printf "\rUpdating %s (%s --> %s)\033[0K\n" \
         $base_name $head_hash $upstream_hash
       git reset --hard --quiet origin/"$def_branch"
       git clean -dfx --quiet
@@ -44,20 +44,20 @@ update_vplugin()
      (( counter++ ))
     fi
   else
-    printf "Warning: %s not installed\033[0K\r" $base_name
+    printf "\rWarning: %s not installed\033[0K]\n" $base_name
   fi
 }
 
 # Update
 source "${0:a:h}/vplugins.sh"
 for plugin in $plugins; do
-  printf "Checking %s\033[0K\r" $plugin
+  printf "\rChecking %s\033[0K" $plugin
   update_vplugin ${${(P)plugin}[2]} ${${(P)plugin}[3]}
 done
 
 # Check that updates occurred
 if [[ "$counter" -eq 0 ]]; then
-  printf "No plugin updates\033[0K\r\n"
+  printf "\rNo plugin updates\033[0K\n"
 else
-  printf "\033[0K\r"
+  printf "\r\033[0K"
 fi
