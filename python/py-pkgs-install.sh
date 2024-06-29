a5a82738c893b838bceb3c4af4ef6c68a9f6f36b
Darren Kavanagh <dkav@users.noreply.github.com>
2024-06-28 22:15:05 -0700

    python: generate ruff shell completions

--- python/py-pkgs-install.sh
+++ python/py-pkgs-install.sh
@@ -9,6 +9,7 @@ if [ -x "$(command -v $HOMEBREW_PREFIX/bin/pipx)" ]; then
 
   # Linting tools
   pipx install ruff pylint vulture
+  ruff generate-shell-completion zsh > $XDG_DATA_HOME/zsh/site-functions/_ruff
 
  # Jupyter
   pipx jupyterlab
