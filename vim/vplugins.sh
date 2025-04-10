06f0495b5ffeaf915abdcc9558f1ad53231d4ef9
Darren Kavanagh <dkav@users.noreply.github.com>
2025-04-10 12:04:11 -0700

    vim: add ai tools and reorder plugins

--- vim/vplugins.sh
+++ vim/vplugins.sh
@@ -2,8 +2,10 @@
 #
 # List of Vim plugins.
 
-plugins=(airline airline_themes flattened nerdtree tagbar gutentags fugitive \
-	commentary surround gitgutter ale ctrlp )
+plugins=(airline airline_themes \
+  flattened \
+  ale commentary fugitive gitgutter gutentags nerdtree surround tagbar \
+  windsurfer copilot ctrlp )
 
 
 # Array=("GitHub repository" "local Vim plugin folder" "Make Vim docs")
@@ -11,18 +13,21 @@ plugins=(airline airline_themes flattened nerdtree tagbar gutentags fugitive \
 # Statusline
 airline=("vim-airline/vim-airline" "statusline/start/airline" true)
 airline_themes=("vim-airline/vim-airline-themes" \
-	"statusline/start/airline-themes" true)
+  "statusline/start/airline-themes" true)
 
 # Themes
 flattened=("romainl/flattened" "themes/opt/flattened" false)
 
 # IDE
-nerdtree=("scrooloose/nerdtree" "ide/start/nerdtree" true)
-tagbar=("majutsushi/tagbar" "ide/start/tagbar" true)
-gutentags=("ludovicchabant/vim-gutentags" "ide/start/gutentags" true)
-fugitive=("tpope/vim-fugitive" "ide/start/fugitive" true)
-commentary=("tpope/vim-commentary" "ide/start/commentary" true)
-surround=("tpope/vim-surround" "ide/start/surround" true)
-gitgutter=("airblade/vim-gitgutter" "ide/start/gitgutter" true)
 ale=("dense-analysis/ale" "ide/start/ale" true)
+commentary=("tpope/vim-commentary" "ide/start/commentary" true)
+fugitive=("tpope/vim-fugitive" "ide/start/fugitive" true)
+gitgutter=("airblade/vim-gitgutter" "ide/start/gitgutter" true)
+gutentags=("ludovicchabant/vim-gutentags" "ide/start/gutentags" true)
+nerdtree=("scrooloose/nerdtree" "ide/start/nerdtree" true)
+surround=("tpope/vim-surround" "ide/start/surround" true)
+tagbar=("majutsushi/tagbar" "ide/start/tagbar" true)
+windsurfer=("Exafunction/windsurf.vim" "ide/start/windsurf" true)
+
+copilot=("github/copilot.vim" "ide/opt/copilot" true)
 ctrlp=("ctrlpvim/ctrlp.vim" "ide/opt/ctrlp" true)
