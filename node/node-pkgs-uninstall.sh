e83cd0a953378d12c4553ce180b15a099eec8f2a
Darren Kavanagh <dkav@users.noreply.github.com>
2025-10-23 13:39:35 -0700

    node: support scoped packages

--- node/node-pkgs-uninstall.sh
+++ node/node-pkgs-uninstall.sh
@@ -4,8 +4,8 @@
 
 if [ -x "$(command -v npm)" ]; then
   echo "Uninstalling Node packages..."
-  npm ls --global --parseable  --depth=0 \
-    | awk -F/ '/node_modules/ && !/\/npm$/ {print $NF}' \
+  npm ls --global --parseable --depth=0 \
+    | awk '/node_modules/ && !/\/npm$/ {sub(".*/node_modules/", ""); print}' \
     | xargs npm uninstall --global --no-progress --quiet 1>/dev/null
 else
   echo "Error: npm is not installed" >&2
