2f5111b1623f153de61cbf65b27dc7bc52c27188
Darren Kavanagh <dkav@users.noreply.github.com>
2026-02-10 09:16:47 -0800

    python: refactor printed output

--- python/py-pkgs-update.sh
+++ python/py-pkgs-update.sh
@@ -4,21 +4,48 @@
 
 function venv_update() {
   # $1 - venv name; $2 - path to req file
-  printf "\nUpdate %s virtual environment\n" $1
-  uvout="$(uv pip install --directory $HOME/.local/pyvenvs/$1 \
-    --exact -U --no-progress -r $2/venv-pydata-reqs.in 2>&1 \
-    | sed -E -n -e '/-|\+/,$p')"
-   if [[ -n $uvout ]]; then
-    echo $uvout
-  else
+  printf "\nUpdate %s virtual environment:\n" $1
+
+  package_updated=false
+  while IFS= read -r line; do
+    if [[ "$line" == *((#i)(error|failed|fatal|critical|warning))* ]]; then
+      echo "$line"
+      break
+    fi
+
+    if [[ "$line" =~ "^ - (.+)==(.+)$" ]]; then
+      package="${match[1]}"
+      old_version="${match[2]}"
+    elif [[ "$line" =~ "^ \+ (.+)==(.+)$" ]] && [[ -n "$package" ]]; then
+      new_version="${match[2]}"
+      echo "$package $old_version->$new_version"
+      package=""
+      package_updated=true
+    fi
+  done < <(uv tool upgrade --all --no-progress --color never 2>&1)
+
+  if [[ "$package_updated" == false ]]; then
     echo "No $1 libraries to update"
   fi
 }
 
 if [ -x "$(command -v $HOMEBREW_PREFIX/bin/uv)" ]; then
   echo "Updating Python Packages..."
-  echo "Update Python apps"
-  uv tool upgrade --all --no-progress --color never
+
+echo "Update Python apps:"
+uv tool upgrade --all --no-progress --color never 2>&1 | while IFS= read -r line; do
+  if [[ "$line" == *((#i)(error|failed|fatal|critical|warning))* ]]; then
+    echo "$line"
+    break
+  fi
+
+  if [[ "$line" =~ "Updated" ]]; then
+    echo "$line"
+  elif [[ "$line" == "Nothing to upgrade" ]]; then
+    echo "No app updates"
+  fi
+done
+
   req_path=${0:a:h}
   venv_update pydata $req_path
 else
