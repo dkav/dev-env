e53702e36423c6c91640b829a9296808c6376264
Darren Kavanagh <dkav@users.noreply.github.com>
2026-02-19 12:05:59 -0800

    ruby: refactor printed output

--- ruby/rb-gems-update.sh
+++ ruby/rb-gems-update.sh
@@ -5,11 +5,22 @@
 if [ -x "$(command -v $HOMEBREW_PREFIX/opt/ruby/bin/bundle)" ]; then
   echo "Updating Ruby Gems..."
   cd ${0:A:h} || return
-  bundle update --all | grep "Installing"
-  ga_update=$?
-  if [[  $ga_update != 0  ]]; then
+
+  ga_update=0
+
+  while IFS= read -r line; do
+    if [[ "$line" == *((#i)(error|failed|fatal|critical|warning))* ]]; then
+      echo "$line"
+    elif [[ "$line" =~ "^Installing ([^ ]+) ([^ ]+) \(was ([^)]+)\)" ]]; then
+      echo "Upgrading ${match[1]} ${match[3]} -> ${match[2]}"
+      ga_update=1
+    fi
+  done < <(bundle update --all)
+
+  if (( !ga_update )); then
       echo "No gems to update"
   fi
+
   bundle clean --force 1>/dev/null
 else
   echo "Error: Bundler is not installed" >&2
