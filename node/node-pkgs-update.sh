fbae0e0d179a690cb0cc198546a0f02d6a5fa9c4
Darren Kavanagh <dkav@users.noreply.github.com>
2026-02-19 12:03:46 -0800

    node: refactor update output

--- node/node-pkgs-update.sh
+++ node/node-pkgs-update.sh
@@ -2,23 +2,21 @@
 #
 # Update outdated global npm packages.
 #
-# Original source -
-# https://gist.github.com/othiym23/4ac31155da23962afd0e
 
 if [ -x "$(command -v npm)" ]; then
   echo "Updating Node Packages..."
-  no=($(sed '/#.*/d;/^$/d' ${0:a:h}/node-requirements.txt \
-      | xargs npm outdated --global --parseable --depth=0 --quiet \
-      | cut -d: -f3,4))
+  no=(${(f)"$(sed '/#.*/d;/^$/d' ${0:a:h}/node-requirements.txt \
+      | xargs npm outdated --global --parseable --depth=0 --quiet)"})
   if [[ -n $no ]]; then
-    for package in $no; do
-      npkg="$(cut -d: -f2 <<<"$package")"
-      cver="$(cut -d@ -f2 <<<"$(cut -d: -f1 <<<"$package")")"
-      pver="$(cut -d@ -f2 <<<"$npkg")"
-      pkg="$(cut -d@ -f1 <<<"$npkg")"
-      echo $npkg
-      echo "Updating $pkg ($cver-->$pver)"
-      npm install --global --no-fund --no-progress --quiet $npkg 1>/dev/null
+    for line in $no; do
+      fields=("${(@s/:/)line}")
+      current=$fields[3]    # @github/copilot-cli@0.0.410
+      latest=$fields[4]     # @github/copilot-cli@0.0.411
+      pkg="${latest%@*}"    # @github/copilot-cli
+      cver="${current##*@}" # 0.0.410
+      pver="${latest##*@}"  # 0.0.411
+      echo "Updating $pkg ($cver --> $pver)"
+      npm install --global --no-fund --no-progress --quiet "$latest" 1>/dev/null
     done
   else
     echo "No packages to update"
