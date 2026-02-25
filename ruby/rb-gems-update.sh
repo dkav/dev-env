7c579b0c750e817e87c4beef8f5f58dcc65b9c7f
Darren Kavanagh <dkav@users.noreply.github.com>
2026-02-24 20:30:28 -0800

    ruby: minor update output changes

--- ruby/rb-gems-update.sh
+++ ruby/rb-gems-update.sh
@@ -6,19 +6,19 @@ if (( $+commands[bundle] )); then
   echo "Updating Ruby Gems..."
   cd ${0:A:h} || return
 
-  ga_update=0
-
+  had_output=false
   while IFS= read -r line; do
     if [[ "$line" == *((#i)(error|failed|fatal|critical|warning))* ]]; then
       echo "$line"
+      had_output=true
     elif [[ "$line" =~ "^Installing ([^ ]+) ([^ ]+) \(was ([^)]+)\)" ]]; then
-      echo "Upgrading ${match[1]} ${match[3]} -> ${match[2]}"
-      ga_update=1
+      echo "${match[1]} ${match[3]} -> ${match[2]}"
+      had_output=true
     fi
   done < <(bundle update --all)
 
-  if (( !ga_update )); then
-      echo "No gems to update"
+  if [[ "$had_output" == false ]]; then
+    echo "No gems to update"
   fi
 
   bundle clean --force 1>/dev/null
