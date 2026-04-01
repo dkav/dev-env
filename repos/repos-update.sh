bdfe33f75c52d1d0dfc6beff90192a023be23381
Darren Kavanagh <dkav@users.noreply.github.com>
2026-04-01 10:26:53 -0700

    repos: simplify repos-update

--- repos/repos-update.sh
+++ repos/repos-update.sh
@@ -1,17 +1,5 @@
 #!/bin/zsh
 
-# Update all repositories (GitHub and Bitbucket).
+# Update all repositories (GitHub).
 
-repos-github sync -q
-
-echo
-repos-github pull
-
-echo
-repos-rebase-worktrees
-
-echo
-repos-push-ssd
-
-echo
-repos-push-bitbucket
+repos-github fetch
