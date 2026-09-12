bf5bedb2fc0076ca8c3e5bc5a646785d3e8e8a9b
Darren Kavanagh <dkav@users.noreply.github.com>
2026-09-12 11:02:50 -0700

    repos: update repositories update workflow

--- repos/repos-update.sh
+++ repos/repos-update.sh
@@ -3,7 +3,15 @@
 # Update all repositories (GitHub and Bitbucket).
 
 repos-github sync -q
+
 echo
-repos-github fetch
+repos-github pull
+
+echo
+repos-rebase-worktrees
+
+echo
+repos-push-ssd
+
 echo
 repos-push-bitbucket
