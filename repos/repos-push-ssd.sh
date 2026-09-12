89b56778202c420a1b03137d29bae45e579d1140
Darren Kavanagh <dkav@users.noreply.github.com>
2026-09-12 10:58:48 -0700

    repos: shfmt fixes

--- repos/repos-push-ssd.sh
+++ repos/repos-push-ssd.sh
@@ -9,12 +9,15 @@ found_repos=0
 echo "Pushing repositories with ssd remote:"
 
 # Check if the drive is connected
-[[ -d "$VOLUME_PATH" ]] || \
-  { echo "Error: $VOLUME_PATH is not connected."; exit 1; }
+[[ -d "$VOLUME_PATH" ]] \
+  || {
+    echo "Error: $VOLUME_PATH is not connected."
+    exit 1
+  }
 
 [[ ! -d "$REPO_DIR" ]] && echo "Error: $REPO_DIR not found." && exit 1
 
-DIRS=( "$REPO_DIR"/*(/) )
+DIRS=("$REPO_DIR"/*(/))
 if [[ ${#DIRS[@]} -eq 0 ]]; then
   echo "Error: No directories found in $REPO_DIR."
   exit 1
@@ -24,14 +27,14 @@ ssd_repos=()
 for dir in "${DIRS[@]}"; do
   [[ -d "$dir/.git" ]] || continue
   found_repos=1
-  git -C "$dir" remote get-url ssd &>/dev/null && ssd_repos+=("$dir")
+  git -C "$dir" remote get-url ssd &> /dev/null && ssd_repos+=("$dir")
 done
 
-for (( i=1; i<=${#ssd_repos[@]}; i++ )); do
+for ((i = 1; i <= ${#ssd_repos[@]}; i++)); do
   dir=${ssd_repos[$i]}
   printf "--- %s ---\n" ${dir:t}
   git -C "$dir" push ssd --force --all
-  (( i < ${#ssd_repos[@]} )) && echo
+  ((i < ${#ssd_repos[@]})) && echo
 done
 
-(( found_repos )) || echo "No Git repositories found in $REPO_DIR."
+((found_repos)) || echo "No Git repositories found in $REPO_DIR."
